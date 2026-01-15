974eeed580223d2d82af1671de91ed8361d77d4e
vibhavkhestabit <vibhavk.hestabit@gmail.com>
2026-01-15 19:58:29 +0530

    storage.js

---
+++ week2/day4/storage.js
@@ -0,0 +1,21 @@
+const STORAGE_KEY = "todos";
+
+export function saveTodos(todos) {
+  try {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+  } catch (error) {
+    throw new Error("LocalStorage save failed");
+  }
+}
+
+
+export function loadTodos() {
+  try {
+    const data = localStorage.getItem(STORAGE_KEY);
+    return data ? JSON.parse(data) : [];
+  } catch (error) {
+    throw new Error("LocalStorage parse failed");
+  }
+}
+
+
